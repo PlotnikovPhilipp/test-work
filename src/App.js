6d41314d2f28adea2a161dacdd4cbbe6afb411ef
Plotnikov Philipp <philipp@MacBook-Pro-Plotnikov.local>
2020-05-23 15:21:37 +0500

    Philipp

--- src/App.js
+++ src/App.js
@@ -4,16 +4,44 @@ import { Column } from './column/column';
 import './board.css';
 
 const amountOfColumns = 4;
+const columnList = [];
+const columnSettings = [
+  {
+    id: 'Наряды',
+    title: 'Наряды',
+    color: 'gray'
+  },
 
+  {
+    id: 'В работе',
+    title: 'В работе',
+    color: 'green'
+  },
+
+  {
+    id: 'Приемка',
+    title: 'Приемка',
+    color: 'orange'
+  },
+
+  {
+    id: 'Завершено',
+    title: 'Завершено',
+    color: 'blue'
+  }
+];
 
 class Board extends React.Component {
   constructor(props) {
-    
+    super(props);
+    for(let i = 0; i < amountOfColumns; i++) {
+      columnList.push(<Column key={ columnSettings[i].id } title={ columnSettings[i].title } color={ columnSettings[i].color }/>);
+    }
   }
 
   render() {
     return(
-      
+      columnList
     );
   }
 }
--- src/column/column.jsx
+++ src/column/column.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import 'column.css';
+import './column.css';
 
 export class Column extends React.Component {
-
 
     render() {
         return(
@@ -12,14 +11,14 @@ export class Column extends React.Component {
                 }
                 <header clasName="column__head">
                     { this.props.title }
-                    <img className="column__add-card" src="../imgs/add-card.svg" alt="add card"/>
+                    <img className="column__add-card" src="../imgs/add-card.png" alt="add card"/>
                 </header>
 
                 {
                     // Main part
                 }
                 <section className="column__body">
-                    
+
                 </section>
             </div>
         );
